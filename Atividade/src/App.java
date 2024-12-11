b26762ca9728d936070aaf547bc731c9cec21bf8
Maria Eduarda de Lima Pinho <12735995437@ifrn.local>
2024-12-10 21:18:39 -0300

    Projeto Pronto!

---
+++ Atividade/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        // Criando uma instância de Pessoa para cada estuantes
+        Pessoa pessoa = new Pessoa("Maria Eduarda de Lima Pinho", 25);
+
+        // Exibindo as informações dos estudantes
+        pessoa.exibirInformacoes();
+    }
+}
